5d35e20bdbea1826c1dd386d44d17b27c769dafc
NathanPearson <nate.m.pearson@gmail.com>
2019-02-13 09:48:16 -0500

    lecture 73 end

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewChecked,
+  AfterViewInit,
+  OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};
-  constructor() { }
 
-  ngOnInit() {
+// demo the lifecycle events of angular
+export class ServerElementComponent implements OnInit, 
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewChecked,
+  AfterViewInit,
+  OnDestroy {
+
+  @Input('srvElement') element: {type: string, name: string, content: string};
+
+  constructor() {
+    console.log('constructor called!');
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('onchange called!');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck() {
+    console.log('docheck called');
+  }
+
+  ngAfterContentInit(): void {
+    // Called after ngOnInit when the component's or directive's content has been initialized.
+    // Add 'implements AfterContentInit' to the class.
+    console.log('afterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('aftercontentchecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('afterviewinit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('afterviewchecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('!!!!!!!!!!!!!!!!!DESTROY!!!!!!!!!!!!!!!!!');
+  }
 }
